ceab4635c78803efbc5d33b455270ef985cd8626
Andrew J Kulpa <ajk142@macadmins-Mac-Pro.local>
2018-07-02 15:34:11 -0500

    initialize repo

---
+++ bot.js
@@ -0,0 +1,50 @@
+var Discord = require('discord.io');
+var auth = require('./auth.json');
+var roller = require('./roller.js');
+var fs = require('fs');
+console.log("Initializing Discord Bot");
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+// bot.connect();
+// console.log(bot.username );
+bot.on('ready', function () {
+    console.log('Connected');
+    console.log('Logged in as: ');
+    console.log(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, event) {
+    try{
+        if (message.substring(0, 1) == '!') {
+            var args = message.substring(1).split(' ');
+            var cmd = args[0];
+        
+            args = args.splice(1);
+            switch(cmd.toLowerCase()) {
+                // !ping
+                case 'roll':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: roller.eval_roll(message.substring(5, message.length).replace(/\s*\[.*?\]\s*/g, ''))
+                    });
+                    break;
+                case 'help':
+                    var message = "Try using '!roll 1d20 + 5' :)"
+                    bot.sendMessage({
+                        to: channelID,
+                        message: message
+                    });
+                    break;
+                default:
+                    console.log("Unknown argument: " + cmd);
+            }
+        }
+    } catch(err) {
+        console.log("Error caught:");
+        console.log(err.message);
+        console.log("\n\n");
+    }
+});
---
+++ roller.js
@@ -0,0 +1,50 @@
+const math = require('mathjs')
+
+module.exports = {
+    eval_roll: function(roll_string){
+        try{
+            const node = math.parse(roll_string);
+            const transformed = node.transform(function (node, path, parent) {
+                var roll_info = rollInfo(node);
+                if (node.isOperatorNode && node.op === '*' && roll_info[0]!=null && roll_info[1]!=null) {
+                    return new math.expression.node.ConstantNode(d(roll_info[0], roll_info[1]));
+                }
+                else {
+                    return node;
+                }
+            });
+            var message = transformed.toString();
+            var message = message + "\n" + math.eval(transformed.toString());
+        } catch {
+            var message = "Error with roll argument string '" + roll_string + "'.";
+        }
+        return message;
+    }
+};
+function rollInfo(node){
+    var lhs = null;
+    var rhs = null;
+    var regex = /[dw]\d+/;
+    node.traverse(function (node, path, parent) {
+        switch (node.type) {
+          case 'ConstantNode':
+            lhs = node.value;
+            break;
+          case 'SymbolNode':
+            rhs = node.name.match(regex)[0].substring(1);
+            op = node.name[0];
+            break;
+        }
+    })
+    return [lhs, rhs, op];
+};
+function random(min, max){
+    return Math.floor(Math.random() * max) + min;
+};
+function d(num_rolls, max){
+    var roll_total = 0;
+    for (i = 0; i < num_rolls; i++){
+        roll_total += random(1, max);
+    }
+    return roll_total;
+};
